1ad0d154958f3fb4ee364e982bc19a46482e611d
Ильмира <kushkarova.1998@mail.ru>
2023-07-09 09:05:47 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author pushkin = new Author("Александр", "Пушкин");
+        Book kapitanskayaDochka = new Book("Капитанская дочка", pushkin, 1836);
+
+        Author lermontov = new Author("Михаил", "Лермонтов");
+        Book geroyNashegovremeny = new Book("Герой нашего времени", lermontov, 1839);
+
+        System.out.println(kapitanskayaDochka.getName());
+        System.out.println(kapitanskayaDochka.getYear());
+        kapitanskayaDochka.setYear(1835);
+        System.out.println(kapitanskayaDochka.getYear());
+
+        System.out.println(geroyNashegovremeny.getName());
+        System.out.println(geroyNashegovremeny.getYear());
+        geroyNashegovremeny.setYear(1800);
+        System.out.println(geroyNashegovremeny.getYear());
     }
 }
